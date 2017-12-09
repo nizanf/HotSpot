20e9cab5bcac52faa8a22ffde9ce8324d5d7c2dc
SimhiGal <33367544+SimhiGal@users.noreply.github.com>
2017-12-08 20:56:45 -0800

    Update purchase class (lock) + more func in views

--- parking/parking/mysite/polls/models.py
+++ parking/parking/mysite/polls/models.py
@@ -6,34 +6,58 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
-
+# for lock 
+import threading
+import time
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	
 	phone_number = models.CharField(max_length=20, blank=True)
-    	score = models.IntegerField(default=50) #start with 50 scores.
-    	rating = models.IntegerField(default=0) # start with rating 0.
+    points 	= models.IntegerField(default=50) #start with 50 points.
+    rating 	= models.IntegerField(default=0) # start with rating 0.
 
+  #   numOfPurchases = models.IntegerField(default=0) # start 0 purcahses
+ 	# numOfSuccessfull = models.IntegerField(default=0)
+ 	# numOfIrrelevent 	 = models.IntegerField(default=0)
+ 	# numOfCanceled 	 = models.IntegerField(default=0)
 
+	
+
+# TODO: Add doc
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
 	if created:
 		Profile.objects.create(user=instance)
 
+# TODO: Add doc
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
   	instance.profile.save()
 
 
-
 class Purchase(models.Model):
-	buy = models.IntegerField(default=-1) # user id
-	sell = models.IntegerField(default=-1) # user id
-    	cost = models.IntegerField(default=10)
-	parking_time = models.DateTimeField()
-	status = models.CharField(max_length=200, default="pending") #pending, done, in process, canceled etc.
-    	parking_rate = models.DecimalField(max_digits = 3, decimal_places=3, default=-1) #distance between target address and parking address. 
-	target_address = models.CharField(max_length=200) #coordinates of target address. (x,y)
+#	purchase_id		= models.IntegerField(default=-1)
+	buyer_id		= models.IntegerField(default=-1) 	# user id
+	seller_id		= models.IntegerField(default=-1) 	# user id
+    cost 			= models.IntegerField(default=10)
+	parking_time 	= models.DateTimeField() #  max 30 minutes from the offering time
+	status 			= models.CharField(max_length=200, default="available") # available, in process, done, canceled irrelevent etc.
+   	parking_rate 	= models.DecimalField(max_digits = 3, decimal_places=3, default=-1) #distance between target address and parking address. 
+	target_address 	= models.CharField(max_length=200) #coordinates of target address. (x,y)
 	parking_address = models.CharField(max_length=200) #coordinates of parking address. (x,y)
+	lock 			= threading.Lock()
+	cond 			= threading.Condition(threading.Lock())
+	pin_code		= models.IntegerField(default=-1)
+
+	def wait_lock(self, timeout):
+	    with self.cond:
+	        current_time = start_time = time.time()
+	        while current_time < start_time + timeout:
+	            if self.lock.acquire(False):
+	                return True
+	            else:
+	                self.cond.wait(timeout - current_time + start_time)
+	                current_time = time.time()
+	    return False
 
--- parking/parking/mysite/polls/views.py
+++ parking/parking/mysite/polls/views.py
@@ -14,127 +14,281 @@ from .models import Profile, Purchase
 from django.core import serializers
 
 import datetime
+# for lock 
+import threading
+import time
+from random import randint
+
+
+
+TIMEOUT_AVAILABLE_PARKING = 20
 
 def login(request):
 	given_username = request.POST.get("username") #in template, div name = username.
 	given_password = request.POST.get("password") #in template, div name = password.
 
-	user = authenticate(username=given_username, password=given_password)
+	auth_code = authenticate(username=given_username, password=given_password)
 
-	if user : #login succeed
+	if auth_code : #login succeed
 		return render(request, 'polls/homepage.html')
 	else: # login failed
-       		return render(request, 'polls/relogin.html')
+       	return render(request, 'polls/relogin.html')
+
 
 def register(request):
-	given_username = request.POST.get("username") #in template, div name = username.
-	given_first_name = request.POST.get("first_name") #in template, div name = first_name.
-	given_last_name = request.POST.get("last_name") #in template, div name = last_name.
-	given_email = request.POST.get("email") #in template, div name = email.
-	given_password = request.POST.get("password") #in template, div name = password.
-	given_phone_number = request.POST.get("phone_number") #in template, div name = phone_number.
+	given_username 		= request.POST.get("username") 		# in template, div name = username.
+	given_first_name 	= request.POST.get("first_name") 	# in template, div name = first_name.
+	given_last_name 	= request.POST.get("last_name") 	# in template, div name = last_name.
+	given_email 		= request.POST.get("email") 		# in template, div name = email.
+	given_password	 	= request.POST.get("password") 		# in template, div name = password.
+	given_phone_number 	= request.POST.get("phone_number") 	# in template, div name = phone_number.
 
-	user = User.objects.create_user(username=given_username, password = given_password, email = given_email, first_name = given_first_name, last_name = given_last_name)
+	new_user = User.objects.create_user(username=given_username, \
+	 		password = given_password, email = given_email, \
+	 		first_name = given_first_name, last_name = given_last_name)
 
-	user.profile.phone_number = given_phone_number
-	user.save()
+	new_user.profile.phone_number = given_phone_number
+	new_user.save()
 
-	user = authenticate(username=given_username, password=given_password)
+	# TODO : Add message: register successfully
+	return render(request, 'polls/login.html')
 
-	return render(request, 'polls/homepage.html')
+# OR - Danny and gal wants to remove
+#	new_user = authenticate(username=given_username, password=given_password)
+#	return render(request, 'polls/homepage.html')
 
 def logout(request):
 	logout(request)
 	return render(request, 'polls/login.html')
 
 def offer_new_parking(request):
-	given_sell = request.user  					# user id
-	given_parking_address =  request.POST.get("location") 	# TODO: verify the nitzans give us the location
-	given_parking_time = request.POST.get("parking_time")
-	purchase = Purchase(sell = given_sell, parking_address = given_parking_address, parking_time = given_parking_time)
+	
+	given_seller_id 		= request.user  					# user id
+	given_parking_address 	= request.POST.get("location") 	# TODO: verify the nitzans give us the location
+	given_parking_time 		= request.POST.get("parking_time")
+	purchase 				= Purchase(	purchase_id =getPurchaseID(), seller_id = given_seller_id,\
+									 	parking_address = given_parking_address, \
+									 	parking_time = given_parking_time)
+
 
 	# TODO: Add event log
-	# TODO: Add debug log
 
 	purchase.save()
+
+	# seller_user			 	= User.objects.get(pk=given_seller_id)
+	# seller_user.numOfPurchases =+1 
+	# seller_user.save()
 
 	return render(request, 'polls/homepage.html')
 
 
 def search_parking(request):
-	given_target_address =  request.POST.get("target_location") 	# TODO: verify the nitzans give us the location
-	radius =  int(request.POST.get("radius")) 	
-	parking_time =  request.POST.get("parking_time") 	# TODO: verify the nitzans give us the location
-	relevant_parkings = get_parkings_by_radius(given_target_address, radius, parking_time)
+	given_target_address 	= request.POST.get("target_location") 	# TODO: verify nitzan give us the location
+	radius 					= int(request.POST.get("radius")) 	
+	parking_time 			= request.POST.get("parking_time") 	# TODO: verify nitzan give us the location
+	relevant_parkings 		= get_parkings_by_radius(given_target_address, radius, parking_time)
 
-	serialize_relevant_parking = serializers.serialize("json", relevant_parkings)
-
-	return render(request, 'polls/show_available_parkings.html', {'relevant_parkings':relevant_parkings})
+	return render(request, 'polls/show_available_parkings.html', { 'relevant_parkings' : relevant_parkings })
 
 
 
 def get_parkings_by_radius(target_address, radius, parking_time):
+	
+	# TODO: Not sure this is all the prakings, why Purchase?
 	all_parkings = Purchase.objects.all()
+
  	relevat_parkings = []
 
   	for parking in all_parkings:
 		current_dist = calculate_distance(parking.parking_address,target_address)
-		if (current_dist<radius and parking_time<parking.pub_date):
+		if (parking_time <= parking.pub_date and \
+			current_dist <= radius and
+			parking.status == "available"):
 			relevat_parkings.append(parking)
 
-	return relevat_parkings
+	serialize_relevant_parking = serializers.serialize("json", relevant_parkings)
+	return serialize_relevant_parking
 
 
 def calculate_distance(address1, address2):
-	return 0 # TO-DO: edit function
+	return 0 # TODO: Implement function
+
+
+def refresh_map(request):
+
+	current_address 	= request.POST.get("current_address")
+	radius 				= int(request.POST.get("radius"))
+	relevant_parkings 	= get_parkings_by_radius(current_address, radius, time.now())
+
+	return render(request, 'polls/show_available_parkings.html', { 'relevant_parkings' : relevant_parking })
+
+
 
 
 def catch_parking(request):
-	given_target_address = request.POST.get("target_address")
-	parking_id = request.POST.get("parking_id")
-	chosen_parking = Purchase.objects.get(pk=parking_id)
+	asked_target_address = request.POST.get("asked_target_address")
+	seller_user	= User.objects.get(pk=chosen_parking.seller_id)
 
-	while (chosen_parking.locked):# TO-DO: add lock properly
-		pass
-	chosen_parking.lock()
+	buyer_user_id 		 = request.user
+	buyer_user 	= User.objects.get(pk=buyer_user_id)
 
-	buy_user = User.objects.get(pk=chosen_parking.buy)
-	sell_user = User.objects.get(pk=chosen_parking.sell)
+	# The radius according to the zoom of the user's map
+	radius 				= request.POST.get("radius")
+	parking_id 			= request.POST.get("parking_id")
+	chosen_parking 		= Purchase.objects.get(pk=parking_id)
 
-	if (buy_user.score < chosen_parking.cost)
+	# Check buyer has enough points
+	if (buyer_user.score < chosen_parking.cost):
 		# TODO: Render to - sorry dude page - No enough points
 		# In the page link to report points
 		return render(request, 'polls/no_points_page.html')
 
+	# If the parking is not free
+	if (chosen_parking.status != "available"):
+		# TOOD: render to unavailable parking dude or pop-up and render to parking list
+		return render(request, 'polls/show_available_parkings.html', \
+			{ 'relevant_parkings' : \
+				get_parkings_by_radius(asked_target_address, radius, time.now()) })
 	
 
-	if (chosen_parking.status == "pending"):
-		chosen_parking.status = "in process"
-		chosen_parking.buy = request.user # get user id
-		chosen_parking.parking_rate = calculate_distance(given_target_address, chosen_parking.parking_address)
-		chosen_parking.target_address = given_target_address
-		chosen_parking.save()
-			
-		buy_user.score -= chosen_parking.cost
-		buy_user.save()
-	else: # Status not pending
-		# TODO: Notification of - Sorry dude - parking not availability
-		# TODO: Render to search page
+	# Lock to edit the object
+	if not chosen_parking.wait_lock(TIMEOUT_AVAILABLE_PARKING):
+
+		# TOOD: render to unavailable parking dude or pop-up and render to parking list
+		return render(request, 'polls/show_available_parkings.html', \
+			{ 'relevant_parkings' : \
+				get_parkings_by_radius(asked_target_address, radius, time.now()) })
+
+
+	chosen_parking.status = "in process"
+	chosen_parking.lock.release()
+	
+	pin_code = generate_new_pin_code() # TODO: Show the buyer the pincode
+
+	rate = calculate_distance(given_target_address, chosen_parking.parking_address)
+	reset_parking(chosen_parking, buyer_user_id, "in process",rate,asked_target_address,pin_code)
+
+	# chosen_parking.pin_code 		= pincode
+	# chosen_parking.status 			= "in process"
+	# chosen_parking.buyer_id 		= buyer_user_id	# get user id
+	# chosen_parking.parking_rate 	= calculate_distance(given_target_address, chosen_parking.parking_address)
+	# chosen_parking.target_address 	= asked_target_address
+	# chosen_parking.save()
+		
+	# In this stage decerase the point only from the buyer 	
+	buyer_user.points -= chosen_parking.cost
+	buyer_user.save()
+
+	return render(request, 'polls/parking_purchased.html')
+
+
+def buyer_cancel_parking(request):
+	
+	buyer_user_id 		= request.user
+	parking_id 			= request.POST.get("parking_id")
+	chosen_parking 		= Purchase.objects.get(pk=parking_id)
+	
+	seller_id 			= chosen_parking.seller_id
+	seller_user			= User.objects.get(pk=seller_id)
+
+	seller.points 	   += chosen_parking.cost
+
+	buyer_user 			=  User.objects.get(pk=buyer_user_id)
+	update_rating_for_user(buyer_user , "cancel")
+
+	#TODO: add lock because the seller may try to cancel
+	chosen_parking.status = "available"
+	chosen_parking.buyer_id = -1
+	chosen_parking.parking_address = models.CharField(max_length=200)
+	chosen_parking.parking_rate = -1
+
+	seller.save()
+	chosen_parking.save()
+
+	return render(request, 'polls/homepage.html')
+
+
+# when seller insert the pincode 
+def make_exchange(request):
+	parking_id 			= request.POST.get("parking_id")
+	offered_parking 	= Purchase.objects.get(pk=parking_id)
+
+	provided_pincode 	= request.POST.get("pincode")
+	parking_pincode		= offered_parking.pincode
+
+	seller_user			= User.objects.get(pk=request.user)
+	buyer_user 			= User.objects.get(pk=offered_parking.buyer_id)
+
+	if (authenticate_pincode(provided_pincode , parking_pincode)) # if authentication succeeded
+		seller.points 			+= chosen_parking.cost
+		offered_parking.status   = 'done'
+
+		# update ratings of the seller and buyer
+		update_rating_for_user(seller_user , "done")
+		update_rating_for_user(buyer_user , "done")
+
+	else
+		# TODO: print authentication failed.. popup screen type again  \ report on buyer
+
+
+def reset_parking(purchase, buyer_id, status,rate,target_address,pincode):
+	purchase.buyer_id			= buyer_id 	# user id
+	purchase.status 			= status
+   	purchase.parking_rate 		= rate  
+	purchase.target_address 	= target_address
+	purchase.pin_code			= pincode
+	offered_parking.save()
+
+#TODO make sure when locking the parking dont need to save the acquiere to the database
+#when to save? is the acquiere recursive?
+def seller_cancel_parking(request):
+
+
+	seller_user			= User.objects.get(pk=request.user)
+	offered_parking_id 	= request.POST.get("parking_id")
+	offered_parking 	= Purchase.objects.get(pk=parking_id)
+	buyer_id 			= offered_parking.buyer_id
+	buyer_user 			= User.objects.get(pk=offered_parking.buyer_id)
+	
+	# Lock to edit the object
+	if offered_parking.wait_lock(TIMEOUT_AVAILABLE_PARKING):
+		if (offered_parking.status == "available"): # no harm done
+			reset_parking(offered_parking, -1, "Irrelevent",-1, None, -1)
+			offered_parking.lock.release()
+			return render(request, 'polls/homepage.html')
+
+		else : #someone already bought the parking
+			reset_parking(offered_parking, buyer_id, "canceled",-1,offered_parking.target_address,-1)
+			offered_parking.lock.release()
+			update_rating_for_user(seller, "canceled")
+			update_rating_for_user(buyer_user, "Irrelevent")
+			seller.points 	   -= (2*chosen_parking.cost) 			#fine seller
+			seller.save()
+			buyer_user.points  += (2*chosen_parking.cost) 			#compensate buyer
+			#TODO: notificate buyer
+			buyer_user.save()
+
+	return render(request, 'polls/homepage.html')
+
+def generate_new_pin_code():
+	return randint(1000, 9999)
 		
 	
+def authenticate_pincode(provided_pincode, actual_pincode):
+	return provided_pincode == actual_pincode
 
 
+def update_rating_for_user(user, status):
 
 
+	# diff = 0
 
+	# switch() {
+	# case "available":
+	# 	diff = 1;
+	# }
 
-
-
-
- 
-
-
-
-
-
+	# user.rating += diff
+	# user.save()
+	pass
 
