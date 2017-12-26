c76ac8b11f13cb516003ad81ec6a2607067c9a1a
orabramov <orabramov3112@gmail.com>
2017-12-26 15:40:29 +0200

    changed in urls.py

--- parking/parking/mysite/mysite/urls.py
+++ parking/parking/mysite/mysite/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
-	#url(r'^polls/', include('polls.urls')),
-	url(r'^admin/', admin.site.urls),
+    url(r'^', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
