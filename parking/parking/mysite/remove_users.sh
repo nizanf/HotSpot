3716f08052873d0460b6f27fa63d890c415be406
dianakolton <danunak2@gmail.com>
2018-01-16 07:13:38 -0800

    add scripts

---
+++ parking/parking/mysite/remove_users.sh
@@ -0,0 +1,16 @@
+import sys
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+#remove all dummy users
+LAST_NAME = "lastName"
+dummy_users = Purchase.objects.filter(last_name = LAST_NAME)
+
+try:
+	for user in dummy_users:
+		user.delete()
+
+except Purchase.DoesNotExist:
+	pass
+
